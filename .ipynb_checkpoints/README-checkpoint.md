67415ee9781934ca0a8f2d404ee4576fe0cf987c
thushara-lakshmanan <thushara_lakshman@yahoo.co.in>
2020-07-10 08:21:08 +0800

    Folder Updated

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,14 @@
+# This is my repository for introduction to python for data analysis
+
+The following folders contain my work for each module of the course.
+
+## Outline for the course
+
+1.Introduction to Python 
+2.Intermediate Python
+3.Geting and Cleaning Data
+4.Data Visualisation
+5.Statistics
+6.Exploratory Data Analysis
+
+
---
+++ lesson_1/my_first_program-1.py
@@ -0,0 +1,42 @@
+## Ramon Perez
+## 02 May 2020
+## Title --> My First Python Program
+
+import time
+
+
+
+print("Hi this is my first Robot-Human interaction")
+
+time.sleep(1)
+
+print("Let's chat for a bit, shall we?")
+
+time.sleep(1)
+
+answer1 = input("Please type your answer here ")
+
+time.sleep(1)
+
+print("My name is Julia Roberts, what's your name?")
+
+time.sleep(1)
+
+answer2 = input("Please type your answer here ")
+
+
+time.sleep(1)
+
+print(f"It is very nice to meet you, {answer2}!")
+
+time.sleep(1)
+
+print("Soooo, what do you do for a living, {}? I am a very famous actrees, just saying (no bragging, at all)!".format(answer2))
+
+time.sleep(1)
+
+answer3 = input("Please type your answer here ")
+
+time.sleep(1)
+
+print("%s sounds like a fun job, can you explain it in layman terms for me please?" % answer3)
